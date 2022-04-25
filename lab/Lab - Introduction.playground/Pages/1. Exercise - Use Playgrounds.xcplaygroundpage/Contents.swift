bf37ea4249775b1c4c77c92ef17577d767d3eb2d
mrkrausestem <33553266+mrkrausestem@users.noreply.github.com>
2022-04-25 12:57:05 -0400

    Added Point Values

--- lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -1,5 +1,7 @@
 /*: 
- ## Exercise - Use Playgrounds
+ ## Exercise - Use Playgrounds.
+ 
+ This exercise is intended to get you comfortable using the programming environment including Schoology, GitHub Desktop and XCODE.
  
  The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
  */
@@ -8,12 +10,12 @@ print("What features make Swift a modern and safe language")
 print("How to use playgrounds to make writing Swift fun and simple")
 print("Where to look for output from my playgrounds when I run them.")
 /*:
- Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
+ Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist. (2 points)
  */
 
 
 /*:
- Use multiple `print` functions to write out some of the lyrics to the song.
+ Use multiple `print` functions to write out some of the lyrics to the song. (3 points)
  */
 
 
